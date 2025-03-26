d3182b8300aa27448658a6293cf5cc59641b1b54
KhamdamovAva <Ava.lifeone@gmail.com>
2025-03-26 14:14:04 +0500

    added components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Main from './components/main'
+import About from './components/About'
+import Technologies from './components/Technologies'
+import Experience from './components/Experience'
 function App() {
 
   return (
-    <>
-      <h1 className=''>saffsa</h1>
-    </>
+    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+      <div className='fixed top-0 -z-10 h-full w-full'>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,#7877c64d,rgba(255,255,255,0))]"></div>
+      </div>
+      <div className='container mx-auto px-8'>
+        <Navbar />
+        <Main />
+        <About />
+        <Technologies />
+        <Experience />
+      </div>
+    </div>
   )
 }
 
