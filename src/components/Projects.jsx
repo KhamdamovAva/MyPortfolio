59da0f74f1a8d19f2bddf5bd8338bf72cc68db11
KhamdamovAva <Ava.lifeone@gmail.com>
2025-05-18 15:21:06 +0500

    added mb to projects - button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -32,7 +32,7 @@ function Projects() {
                   alt={project.title}
                 />
               </a>
-              <button className='w-[100px] p-[4px] bg-neutral-900 text-purple-700 rounded-[5px]'><a href={project.link}>{t('project.LinkBtn')}</a></button>
+              <button className='mb-[40px] w-[100px] p-[4px] bg-neutral-900 text-purple-700 rounded-[5px]'><a href={project.link}>{t('project.LinkBtn')}</a></button>
             </motion.div>
 
             <motion.div
