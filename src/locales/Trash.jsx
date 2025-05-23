15415b503b307b920971665be7fc1f0a62dc045c
KhamdamovAva <Ava.lifeone@gmail.com>
2025-05-23 15:20:20 +0500

    removed a project

---
+++ src/locales/Trash.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Trash() {
+  return (
+    <>
+      {/* {
+        "title": "PC Shop Website",
+      "description": "A complex project where I worked with a team on GitHub. The platform includes a wide range of features such as product filtering, currency conversion, site translation, user registration, order processing, product selection and descriptions, modal windows, animations, product search, sliders, and more.",
+      "technologies": ["HTML", "Tailwind", "React", "Vite", "API", "Git/GitHub Team"],
+      "link": "https://ingamehub.vercel.app/"
+    }, */}
+
+      {/* {
+        "title": "Вебсайт магазина ПК",
+      "description": "Сложный проект, над которым я работал с командой на GitHub. Платформа включает в себя множество функций, таких как фильтрация товаров, конвертация валюты, перевод сайта, регистрация пользователей, обработка заказов, выбор и описание товаров, модальные окна, анимации, поиск товаров, слайдеры и многое другое.",
+      "technologies": ["HTML", "Tailwind", "React", "Vite", "API", "Git/GitHub Team"],
+      "link": "https://ingamehub.vercel.app/"
+  }, */}
+
+      {/* {
+        "title": "PC Shop Website",
+      "description": "Bu jamoada GitHub-da ishlagan murakkab loyiha. Platformada mahsulotni filtrlash, valuta konvertatsiyasi, saytni tarjima qilish, foydalanuvchi ro'yxatdan o'tish, buyurtma qayta ishlash, mahsulot tanlash va tavsiflari, modal oynalar, animatsiyalar, mahsulot izlash, slaydlar va boshqa bir qator xususiyatlar mavjud.",
+      "technologies": ["HTML", "Tailwind", "React", "Vite", "API", "Git/GitHub Team"],
+      "link": "https://ingamehub.vercel.app/"
+  }, */}
+    </>
+  )
+}
+
+export default Trash
--- src/projectsImages.jsx
+++ src/projectsImages.jsx
@@ -1,11 +1,11 @@
-import project1 from "./assets/projects/project-1.png";
+// import project1 from "./assets/projects/project-1.png";
 import project2 from "./assets/projects/project-2.png";
 import project3 from "./assets/projects/project-3.png";
 import project4 from "./assets/projects/project-4.png";
 import project5 from "./assets/projects/project-5.png";
 
 export const PROJECT_IMAGES = [
-  project1,
+  // project1,
   project2,
   project3,
   project4,
