2e46036399fcb1e4695caca957231be6c50c076a
KhamdamovAva <Ava.lifeone@gmail.com>
2025-06-18 15:05:41 +0500

    added a new project

--- src/projectsImages.jsx
+++ src/projectsImages.jsx
@@ -3,9 +3,11 @@ import project2 from "./assets/projects/project-2.png";
 import project3 from "./assets/projects/project-3.png";
 import project4 from "./assets/projects/project-4.png";
 import project5 from "./assets/projects/project-5.png";
+import project6 from "./assets/projects/project-6.png";
 
 export const PROJECT_IMAGES = [
   // project1,
+  project6,
   project2,
   project3,
   project4,
